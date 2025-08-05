86e4d77230f210085a41eddde022ed3a68b736ab
Farhan <ullah8113@gmail.com>
2025-08-06 00:10:55 +0600

    Folder structure of Controller and Services added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require;
 const app = new express();
+const router = require("./routes/index");
 
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
@@ -11,9 +12,30 @@ const mongoose = require("mongoose");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 
-let URL = "mongodb://localhost:27017/ecommerce";
+let URL = "mongodb+srv://ecommerce:ecommerce@ecommerce-backend.9pqs1cz.mongodb.net/?retryWrites=true&w=majority&appName=Ecommerce-Backend";
 let option = { user: "testuser777", pass: "testuser777", autoIndex: true };
 mongoose
   .connect(URL, option)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
+
+
+app.use(cookieParser())
+app.use(helmet())
+app.use(mongoSanitize());
+app.use(xssClean());
+app.use(hpp());
+app.use(cors());
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+app.use("/api/v1",router)
+
+app.use(express.static('client/dist'));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+});
+
+module.exports = app;
